c433e89023035e31337b48e60d49feabfac0c9bd
lastbenchcoder <42708436+lastbenchcoder@users.noreply.github.com>
2018-10-26 22:38:28 +0530

    stores table column name updated

--- Rachna.Teracotta.Project.Source/Models/Stores.cs
+++ Rachna.Teracotta.Project.Source/Models/Stores.cs
@@ -11,20 +11,29 @@ namespace Rachna.Teracotta.Project.Source.Models
     public class Stores
     {
         [Key]
+        [Column("store_id")]
         public int Store_Id { get; set; }
         [MaxLength(50)]
+        [Column("store_code")]
         public string StoreCode { get; set; }
         [MaxLength(200)]
+        [Column("name")]
         public string Store_Name { get; set; }
         [MaxLength(450)]
+        [Column("description")]
         public string Store_Description { get; set; }
         [MaxLength(450)]
+        [Column("url")]
         public string Store_Url { get; set; }
         [MaxLength(450)]
+        [Column("banner")]
         public string Store_Logo { get; set; }
+        [Column("datecreated")]
         public DateTime Store_CreatedDate { get; set; }
+        [Column("dateupdated")]
         public DateTime Store_UpdatedDate { get; set; }
         [MaxLength(15)]
+        [Column("status")]
         public string Store_Status { get; set; }
         [NotMapped]
         public int TotProducts { get; set; }
--- Rachna.Teracotta.Project.Source/Models/Stores_Audit.cs
+++ Rachna.Teracotta.Project.Source/Models/Stores_Audit.cs
@@ -11,21 +11,31 @@ namespace Rachna.Teracotta.Project.Source.Models
     public class Stores_Audit
     {
         [Key]
+        [Column("store_audit_id")]
         public int Audit_Store_Id { get; set; }
+        [Column("store_id")]
         public int Store_Id { get; set; }
         [MaxLength(50)]
+        [Column("store_code")]
         public string StoreCode { get; set; }
         [MaxLength(200)]
+        [Column("name")]
         public string Store_Name { get; set; }
         [MaxLength(450)]
+        [Column("description")]
+        public string Store_Description { get; set; }
+        [MaxLength(450)]
+        [Column("url")]
         public string Store_Url { get; set; }
         [MaxLength(450)]
+        [Column("banner")]
         public string Store_Logo { get; set; }
+        [Column("datecreated")]
         public DateTime Store_CreatedDate { get; set; }
+        [Column("dateupdated")]
         public DateTime Store_UpdatedDate { get; set; }
         [MaxLength(15)]
+        [Column("status")]
         public string Store_Status { get; set; }
-        public string Action { get; set; }
-        public string DataUser { get; set; }
     }
 }
