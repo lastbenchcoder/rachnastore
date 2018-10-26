467a63df4a59584911085c7945ba629527974b6e
lastbenchcoder <42708436+lastbenchcoder@users.noreply.github.com>
2018-10-26 22:56:17 +0530

    invitation table column names updated

--- Rachna.Teracotta.Project.Source/Models/Invitations.cs
+++ Rachna.Teracotta.Project.Source/Models/Invitations.cs
@@ -10,20 +10,26 @@ namespace Rachna.Teracotta.Project.Source.Models
     public class Invitations
     {
         [Key]
-        public int Invitation_Id { get; set; }        
-        [MaxLength(15)]
-        public string Role { get; set; }
-        public int Store_Id { get; set; }
+        [Column("invitation_id")]
+        public int Invitation_Id { get; set; }
         [MaxLength(50)]
+        [Column("invitation_code")]
         public string Invitation_Code { get; set; }
+        [MaxLength(15)]
+        [Column("role")]
+        public string Role { get; set; }
+        [Column("store_id")]
+        public int Store_Id { get; set; }
         [MaxLength(300)]
+        [Column("emailid")]
         public string Invitation_EmailId { get; set; }
         [MaxLength(15)]
+        [Column("status")]
         public string Invitation_Status { get; set; }
+        [Column("datecreated")]
         public DateTime Invitation_CreatedDate { get; set; }
+        [Column("dateupdated")]
         public DateTime Invitation_UpdatedDate { get; set; }
-        [NotMapped]
-        public string Action { get; set; }
         [NotMapped]
         public string ErrorMessage { get; set; }
 
