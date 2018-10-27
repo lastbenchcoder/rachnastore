1ff4919fe4f4ef9f2d6415bd2a68e9aa31a6dfee
lastbenchcoder <42708436+lastbenchcoder@users.noreply.github.com>
2018-10-27 13:05:47 +0530

    admincolumn name updated

--- Rachna.Teracotta.Project.Source/Models/AdminChatting.cs
+++ Rachna.Teracotta.Project.Source/Models/AdminChatting.cs
@@ -10,11 +10,16 @@ namespace Rachna.Teracotta.Project.Source.Models
     public class AdminChatting
     {
         [Key]
+        [Column("admin_chat_id")]
         public int Admin_Chatting_Id { get; set; }
+        [Column("admin_id")]
         public int Administrators_Id { get; set; }
         [MaxLength(450)]
+        [Column("message")]
         public string Message { get; set; }
+        [Column("datecreated")]
         public DateTime CreatedDate { get; set; }
+        [Column("dateupdated")]
         public DateTime UpdatedDate { get; set; }
         [NotMapped]
         public string ErrorMessage;
--- Rachna.Teracotta.Project.Source/Models/Administrator.cs
+++ Rachna.Teracotta.Project.Source/Models/Administrator.cs
@@ -10,28 +10,43 @@ namespace Rachna.Teracotta.Project.Source.Models
     {
 
         [Key]
+        [Column("admin_id")]
         public int Administrators_Id { get; set; }
+        [Column("invitation_id")]
         public int Invitation_Id { get; set; }
+        [Column("store_id")]
         public int Store_Id { get; set; }
         [MaxLength(50)]
+        [Column("admin_code")]
         public string AdminCode { get; set; }
         [MaxLength(100)]
+        [Column("fullname")]
         public string FullName { get; set; }
         [MaxLength(300)]
+        [Column("emailid")]
         public string EmailId { get; set; }
         [MaxLength(15)]
+        [Column("phone")]
         public string Phone { get; set; }
         [MaxLength(450)]
+        [Column("description")]
         public string Description { get; set; }
         [MaxLength(450)]
+        [Column("photo")]
         public string Photo { get; set; }
         [MaxLength(15)]
+        [Column("password")]
         public string Password { get; set; }
         [MaxLength(15)]
+        [Column("status")]
         public string Admin_Status { get; set; }
+        [Column("role")]
         public string Admin_Role { get; set; }
+        [Column("datecreated")]
         public DateTime Admin_CreatedDate { get; set; }
+        [Column("dateupdated")]
         public DateTime Admin_UpdatedDate { get; set; }
+        [Column("login_attempt")]
         public int Admin_Login_Attempt { get; set; }
         [NotMapped]
         public string ErrorMessage;
